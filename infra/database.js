86b1d7db35430964a7ffdccf3f5f32e3f90a7ea1
Hugo Nogueira Koketu <96093334+hkokt@users.noreply.github.com>
2024-02-07 15:26:56 +0000

    production logs for `database.js`

--- infra/database.js
+++ infra/database.js
@@ -10,6 +10,14 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD
   });
 
+  console.log('POSTGRES_CREDENTIALS:', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
+
   try {
     await client.connect();
     const res = await client.query(queryObject);
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -14,5 +14,4 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(database.max_connections).toEqual(100);
   expect(database.opened_connections).toEqual(1);
   expect(database.version).toEqual('16.0');
-
 });
