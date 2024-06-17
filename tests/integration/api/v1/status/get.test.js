73fd9fff08c31ac0132a802a1ccd280275db1ac0
Hugo Nogueira Koketu <96093334+hkokt@users.noreply.github.com>
2024-06-17 19:39:49 +0000

    added new method to handle ssl connection

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -10,7 +10,7 @@ test("GET to /api/v1/status should return 200", async () => {
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
   const database = responseBody.dependencies.database;
-
+  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", responseBody)
   expect(database.max_connections).toEqual(100);
   expect(database.opened_connections).toEqual(1);
   expect(database.version).toEqual('16.0');
