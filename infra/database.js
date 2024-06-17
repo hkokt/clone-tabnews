428784fa07911d43ba02ed128c287741765fd873
Hugo Nogueira Koketu <96093334+hkokt@users.noreply.github.com>
2024-06-17 19:02:00 +0000

    hadle db ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,15 +7,8 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-  });
-
-  console.log('POSTGRES_CREDENTIALS:', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === 'development' ? false : true
   });
 
   try {
